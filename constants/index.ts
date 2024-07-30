12527f39977867b3685d10ca8f6deb671e3ce3db
ddemolin <damien.demolin@atos.net>
2024-07-30 14:38:16 +0200

    feat: implement desktop sidebar

--- constants/index.ts
+++ constants/index.ts
@@ -1,1 +1,22 @@
-
+export const sidebarLinks = [
+  {
+    imgURL: "/icons/home.svg",
+    route: "/",
+    label: "home",
+  },
+  {
+    imgURL: "/icons/dollar-circle.svg",
+    route: "/my-banks",
+    label: "myBanks",
+  },
+  {
+    imgURL: "/icons/transaction.svg",
+    route: "/transaction-history",
+    label: "transactionHistory",
+  },
+  {
+    imgURL: "/icons/money-send.svg",
+    route: "/payment-transfer",
+    label: "transferFunds",
+  },
+];
--- utils/index.ts
+++ utils/index.ts
@@ -1,3 +1,6 @@
+import { twMerge } from "tailwind-merge";
+import { type ClassValue, clsx } from "clsx";
+
 export function formatAmount(amount: number, currency: string = "EUR"): string {
     const formatter = new Intl.NumberFormat("fr-FR", {
       style: "currency",
@@ -7,4 +10,8 @@ export function formatAmount(amount: number, currency: string = "EUR"): string {
   
     return formatter.format(amount);
   }
+
+  export function cn(...inputs: ClassValue[]) {
+    return twMerge(clsx(inputs));
+  }  
   
