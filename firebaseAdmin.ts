43b3c8a39a2502d97f3d627298390e618331e9a2
ddemolin <damien.demolin@atos.net>
2024-08-27 17:24:01 +0200

    feat: update env

--- firebaseAdmin.ts
+++ firebaseAdmin.ts
@@ -1,8 +1,11 @@
 import { initializeApp, cert, getApps } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
 
+const serviceAccount = JSON.parse(
+  process.env.FIREBASE_SERVICE_ACCOUNT_KEY || "{}"
+);
 const firebaseAdminConfig = {
-  credential: cert("./service-account-file.json"),
+  credential: cert(serviceAccount),
 };
 
 if (getApps().length <= 0) {
